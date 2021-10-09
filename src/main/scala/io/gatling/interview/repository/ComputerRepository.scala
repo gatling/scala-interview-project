4d075f284a57045b89ebb6c96931112772be9e2e
Bastien Amar <bastien.amar@gurb.org>
2021-10-09 16:03:48 +0200

    :card_file_box: Mock a read / write repository

--- src/main/scala/io/gatling/interview/repository/ComputerRepository.scala
+++ src/main/scala/io/gatling/interview/repository/ComputerRepository.scala
@@ -8,8 +8,13 @@ import java.time.LocalDate
 
 class ComputerRepository[F[_]](implicit F: Sync[F]) {
   def fetchAll(): F[Seq[Computer]] = F.pure(
-    Seq(Computer(1L, "Laptop", Some(LocalDate.of(1970, 10, 25)), None), Computer(2L, "toto", Some(LocalDate.of(1989, 10, 5)), Some(LocalDate.of(1998, 5, 15))))
+    ComputerRepository.dbComputer
   )
 
-  def insert(): F[Unit] = F.unit
+  def insert(c: Computer): F[Unit] =
+    F.pure(ComputerRepository.dbComputer =  ComputerRepository.dbComputer :+ c)
+}
+
+object ComputerRepository {
+  var dbComputer = Seq(Computer(1L, "Laptop", Some(LocalDate.of(1970, 10, 25)), None), Computer(2L, "toto", Some(LocalDate.of(1989, 10, 5)), Some(LocalDate.of(1998, 5, 15))))
 }
