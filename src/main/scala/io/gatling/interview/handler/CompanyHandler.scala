a5fbece4849ff5992e69b25cb06bf11eabbb2320
Bastien Amar <bastien.amar@gurb.org>
2021-10-10 10:30:15 +0200

    :sparkles: Make get company work

--- src/main/scala/io/gatling/interview/handler/CompanyHandler.scala
+++ src/main/scala/io/gatling/interview/handler/CompanyHandler.scala
@@ -2,10 +2,14 @@ package io.gatling.interview.handler
 
 import cats.effect.Sync
 import io.finch.{ Ok, Output }
-import io.gatling.interview.repository.CompanyRepository
 import io.gatling.interview.model.Company
+import io.gatling.interview.repository.CompanyRepository
+import cats.implicits._
+
 
 class CompanyHandler[F[_]](companyRepository: CompanyRepository[F])(implicit F: Sync[F]) {
-  def queryComputers(): F[Output[Seq[Company]]] =
-    companyRepository.fetchAll().map(Ok)
+  def queryCompany(): F[Output[Seq[Company]]] =
+    companyRepository
+      .fetchAll()
+      .map(Ok)
 }
